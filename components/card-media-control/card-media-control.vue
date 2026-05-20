5995515800d5745e5081aa99981855a1158c9812
Xiaodong Zhuang <xiaodong.zhuang@hotmail.com>
2026-05-21 00:49:35 +0800

    添加滑动条设置录制时长

--- components/card-media-control/card-media-control.vue
+++ components/card-media-control/card-media-control.vue
@@ -13,28 +13,33 @@
 					停止录像
 				</button>
 			</view>
-			<view class="general-card">
-				<button class="media-btn btn" type="primary" :disabled="isSending" @click="$emit('setDuration', 0)">
-					15s
-				</button>
-				<button class="media-btn btn" type="primary" :disabled="isSending" @click="$emit('setDuration', 1)">
-					1min
-				</button>
-				<button class="media-btn btn" type="primary" :disabled="isSending" @click="$emit('setDuration', 2)">
-					3min
-				</button>
-				<button class="media-btn btn" type="primary" :disabled="isSending" @click="$emit('setDuration', 3)">
-					5min
-				</button>
-				<button class="media-btn btn" type="primary" :disabled="isSending" @click="$emit('setDuration', 4)">
-					10min
-				</button>
+			<view class="duration-control">
+				<view class="duration-label">
+					<text>录制时长</text>
+					<text class="duration-value">{{ durationOptions[currentDuration] }}</text>
+				</view>
+				<slider 
+					class="duration-slider"
+					:value="currentDuration"
+					:min="0"
+					:max="4"
+					:step="1"
+					:disabled="isSending"
+					@change="handleDurationChange"
+				/>
+				<view class="duration-marks">
+					<view v-for="(label, index) in durationOptions" :key="index" class="mark">
+						{{ label }}
+					</view>
+				</view>
 			</view>
 		</view>
 	</view>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
 defineProps({
 	isSending: {
 		type: Boolean,
@@ -42,7 +47,15 @@ defineProps({
 	}
 })
 
-defineEmits(['takePhoto', 'startRecording', 'stopRecording', 'setDuration'])
+const emit = defineEmits(['takePhoto', 'startRecording', 'stopRecording', 'setDuration'])
+
+const durationOptions = ['15秒', '1分钟', '3分钟', '5分钟', '10分钟']
+const currentDuration = ref(0)
+
+const handleDurationChange = (event) => {
+	currentDuration.value = event.detail.value
+	emit('setDuration', currentDuration.value)
+}
 </script>
 
 <style lang="scss" scoped>
@@ -101,5 +114,53 @@ button {
 	display: flex;
 	align-items: center;
 	justify-content: center;
+}
+
+.duration-control {
+	display: flex;
+	flex-direction: column;
+	gap: 12rpx;
+	margin-top: 20rpx;
+	padding: 20rpx;
+	background: #f5f7fb;
+	border-radius: 16rpx;
+}
+
+.duration-label {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	font-size: 28rpx;
+	color: #1f2f4d;
+	font-weight: 500;
+}
+
+.duration-value {
+	font-size: 32rpx;
+	font-weight: 600;
+	color: #5a7cfa;
+}
+
+.duration-slider {
+	width: 80%;
+	margin: 20rpx auto 0 auto;
+}
+
+.duration-marks {
+	display: grid;
+	grid-template-columns: repeat(5, 1fr);
+	margin-top: 12rpx;
+	width: 100%;
+	margin-left: auto;
+	margin-right: auto;
+	gap: 0;
+	padding: 0;
+}
+
+.mark {
+	font-size: 22rpx;
+	color: #999;
+	text-align: center;
+	flex: none;
 }
 </style>
